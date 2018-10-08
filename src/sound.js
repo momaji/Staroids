830aa460117a22d0e9db607183ed38b5fa3b2a54
Jason Nagy <nagyj2@mcmaster.ca>
2018-10-08 09:29:26 -0400

    Added sound code of old for reference

---
+++ src/sound.js
@@ -0,0 +1,38 @@
+
+
+//Below is for reference only
+// Sound = {
+//   laser:     new Audio('39459__THE_bizniss__laser.wav'),
+//   explosion: new Audio('51467__smcameron__missile_explosion.wav')
+// };
+
+// preload audio
+// for (var sfx in SFX) {
+//   (function () {
+//     var audio = SFX[sfx];
+//     audio.muted = true;
+//     audio.play();
+//
+//     SFX[sfx] = function () {
+//       if (!this.muted) {
+//         if (audio.duration == 0) {
+//           // somehow dropped out
+//           audio.load();
+//           audio.play();
+//         } else {
+//           audio.muted = false;
+//           audio.currentTime = 0;
+//         }
+//       }
+//       return audio;
+//     }
+//   })();
+// }
+// // pre-mute audio
+// SFX.muted = true;
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 200);
+ctx.stroke();
