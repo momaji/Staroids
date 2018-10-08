bdf3925295a5163585841f1e1c257669824a3b7b
Jason Nagy <nagyj2@mcmaster.ca>
2018-10-08 09:31:15 -0400

    Importation method of the various .js files

---
+++ src/appendHead.js
@@ -0,0 +1,11 @@
+window.onload = dynamicallyLoadScript("utilities.js")
+window.onload = dynamicallyLoadScript("gameobject.js")
+window.onload = dynamicallyLoadScript("sound.js")
+window.onload = dynamicallyLoadScript("gamestate.js")
+
+function dynamicallyLoadScript(url) {
+  var script = document.createElement("script");  // sake a script DOM node
+  script.src = url;  // set its src to the provided URL
+
+  document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+}
