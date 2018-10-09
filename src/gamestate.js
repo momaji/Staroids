290f657e3219d9dcd066dbf45b19542826efec8f
Jason Nagy <nagyj2@mcmaster.ca>
2018-10-09 16:38:56 -0400

    Added input support

--- src/gamestate.js
+++ src/gamestate.js
@@ -1,33 +1,56 @@
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(100, 0);
+ctx.stroke();
+
 Game = {
   score: 0,
   lives: 0,
   canvasWidth: 780,
-  canvasHeight 540,
+  canvasHeight: 540,
   sprites: [],
   player: null,
   alien: null,
   
-  state: {
+  fsm: {
     start: function(){}, //Initializes all code and systems
     pregame: function(){}, //The default screen
     load: function(){}, //randomizes level
     playing: function(){}, //playing screen
     postgame: function(){}, //game over screen
     pause: function(){}, //paused game
+    execute: function(){this[this.state]();}, //plays whatever "state" is set to
     state:"start"
   }
 }
 
-//Main game loop
-$(function () {
-  var canvas = $("#canvas");
-  Game.canvasWidth  = canvas.width();
-  Game.canvasHeight = canvas.height();
-  
-});
+window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
+window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
 
-var c = document.getElementById("canvas");
-var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(100, 0);
-ctx.stroke();
+//Creates simple game loop
+setInterval(onTimerTick, 33); // 33 milliseconds = ~ 30 frames per sec
+function onTimerTick() {
+  document.getElementById("output1").innerHTML = Key.isDown(Key.UP); //simple output
+  document.getElementById("output2").innerHTML = Key.isDown(Key.DOWN);
+  document.getElementById("output3").innerHTML = Key.isDown(Key.LEFT);
+  document.getElementById("output4").innerHTML = Key.isDown(Key.RIGHT);
+}
+
+//Main game loop
+// $(function () {
+//   var canvas = $("#canvas");
+//   Game.canvasWidth  = canvas.width();
+//   Game.canvasHeight = canvas.height();
+//   var context = canvas[0].getContext("2d");
+//
+//   // while (true){
+//   alert(Key.isDown(Key.UP));
+//   if (Key.isDown(Key.UP)){
+//     ctx.moveTo(0, 0);
+//     ctx.lineTo(1000 * Math.random(), 1000 * Math.random());
+//     ctx.stroke();
+//     alert("yo");
+//   }
+//   // }
+// });
--- src/utilities.js
+++ src/utilities.js
@@ -2,6 +2,27 @@ distance = function(a,b){
   return Math.sqrt(Math.pow(a.x-b.x,2)+Math.pow(a.y-b.y,2))
 }
 
+var Key = {
+  pressed: {},
+
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN:40,
+
+  isDown: function(keyCode){
+    return this.pressed[keyCode];
+  },
+
+  onKeydown: function(event){
+    this.pressed[event.keyCode]=(new Date).getTime();
+  },
+
+  onKeyup: function(event){
+    delete this.pressed[event.keyCode];
+  }
+};
+
 Matrix = function(){}; //determine how this works
 
 GridNode = function(){}; //determine how it works
