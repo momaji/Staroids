85a97dd277d3a952b8089679e015c422bd32d03a
Jason Nagy <nagyj2@mcmaster.ca>
2018-11-07 19:57:51 -0500

    Added missing documentation

--- src/Head.js
+++ src/Head.js
@@ -3,7 +3,10 @@ window.onload = dynamicallyLoadScript("sound.js")
 window.onload = dynamicallyLoadScript("gameobject.js")
 window.onload = dynamicallyLoadScript("gamestate.js")
 
+/** Loads multiple JavaScript files into a large virtual JavaScript file. Allows for the modules to exist within seperate files and then get put together. */
 function dynamicallyLoadScript(url) {
+  // Taken from the following link
+  //https://stackoverflow.com/questions/950087/how-do-i-include-a-javascript-file-in-another-javascript-file
   var script = document.createElement("script");  // sake a script DOM node
   script.src = url;  // set its src to the provided URL
 
